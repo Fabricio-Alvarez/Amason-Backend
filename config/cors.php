6f0bcfb4cbf9e4f1c7f19c8c11e6c797023898aa
Fabricio-Alvarez <jerry.alvarez@ucr.ac.cr>
2024-12-24 20:57:40 -0600

    .

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('https://amason-store.vercel.app'),
+    'allowed_origins' => [env('https://amason-store.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
