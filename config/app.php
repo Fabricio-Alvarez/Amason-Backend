c795533bb8af2dbe22c9e5579e95bff342327b8f
Fabricio-Alvarez <jerry.alvarez@ucr.ac.cr>
2024-12-25 06:51:42 -0600

    .

--- config/app.php
+++ config/app.php
@@ -55,9 +55,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://amason-backend-production.up.railway.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_url' => env('FRONTEND_URL', 'https://amason-store.vercel.app'),
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
     'asset_url' => env('ASSET_URL'),
 
